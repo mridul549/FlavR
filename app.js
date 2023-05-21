92de5791eb6f51438887f246911fc161d4d364d4
Mridul Verma <94969636+mridul549@users.noreply.github.com>
2023-05-21 16:38:04 +0530

    add outlet schema

--- app.js
+++ app.js
@@ -1,6 +1,28 @@
-const express  = require('express');
+const express    = require('express');
+const mongoose   = require('mongoose');
+const bodyParser = require('body-parser');
+const morgan     = require('morgan');
 
 const app = express();
+mongoose.connect("mongodb+srv://mridul549:xTKgkDyitxpKcOY7@cluster0.iuoe1mb.mongodb.net/?retryWrites=true&w=majority")
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+
+// implementing CORS security mechanism
+app.use((req,res,next) => {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    );
+    if(req.method === "OPTIONS") {
+        res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
+        return res.status(200).json({});
+    }
+    next();
+})
 
 app.use('/', require('./routes/home'));
 
