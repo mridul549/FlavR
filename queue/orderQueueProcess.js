6399671f2fb9a1e167da676874163a4d91892089
Mridul Verma <94969636+mridul549@users.noreply.github.com>
2023-06-01 18:05:29 +0530

    Queue processor

---
+++ queue/orderQueueProcess.js
@@ -0,0 +1,32 @@
+const { default: mongoose, model } = require('mongoose');
+const Order    = require('../models/order')
+const Seq      = require('../models/seq')
+
+mongoose.connect("mongodb+srv://mridul549:xTKgkDyitxpKcOY7@cluster0.iuoe1mb.mongodb.net/?retryWrites=true&w=majority")
+
+const orderQueueProcess = async (job, done) => {
+    const orderid = job.data.orderid
+
+    Seq.findOneAndUpdate({ key: "Counter_key" }, { $inc: { "counter": 1 } }, { new: true })
+    .exec()
+    .then(async result => {
+        let orderNum = result.counter
+
+        await Order.updateOne({ _id: orderid }, { 
+            $set: { orderNumber: orderNum, status: "preparing" }
+        })
+        .exec()
+        return result
+    })
+    .then(result => {
+        done()
+    })
+    .catch(err => {
+        console.log(err);
+        return res.status(500).json({
+            error: err
+        })
+    })
+}
+
+module.exports = orderQueueProcess
