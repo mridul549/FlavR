50f5c0104c21be382a195d94c692c6ffecd74821
Mridul Verma <94969636+mridul549@users.noreply.github.com>
2023-06-01 17:53:08 +0530

    Hide secret

--- middlewares/check-auth.js
+++ middlewares/check-auth.js
@@ -3,7 +3,7 @@ const jwt = require('jsonwebtoken');
 module.exports = (req,res,next) => {
     try {
         const token = req.headers.authorization.split(" ")[1];
-        const decoded = jwt.verify(token, "nescafeAppSecretKey");
+        const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
 
         req.userData = decoded;
         next();
