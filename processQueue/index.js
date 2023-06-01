27e24e9fb1f0ec431911dbc4000c43e1342c8f94
Mridul Verma <94969636+mridul549@users.noreply.github.com>
2023-06-01 12:48:18 +0530

    Connect to redis cloud

--- processQueue/index.js
+++ processQueue/index.js
@@ -1,32 +1,29 @@
 const Queue = require('bull')
 const path  = require('path')
-const { REDIS_PORT, REDIS_URI, REDIS_PASSWORD } = require('../config/redis')
+const { REDIS_PORT, REDIS_URI, REDIS_PASSWORD, REDIS_HOST } = require('../config/redis')
 const { Connect } = require("taskforce-connector");
 const redis = require('redis');
 
 const client = redis.createClient({
-    password: 'zvrM2aS77e1WPps4D4gO6u4cIB1L5Jmo',
-    socket: {
-        host: 'redis-10818.c273.us-east-1-2.ec2.cloud.redislabs.com',
-        port: 10818
-    }
+    url: "redis://default:zvrM2aS77e1WPps4D4gO6u4cIB1L5Jmo@redis-10818.c273.us-east-1-2.ec2.cloud.redislabs.com:10818",
 });
 
+client.connect();
+
+// Event listener for Redis errors
 client.on('connect', () => {
     console.log('Connected to Redis Cloud');
 });
   
-// Event listener for Redis errors
 client.on('error', (err) => {
     console.error('Redis Error:', err);
 });
   
-
 const orderQueue = new Queue('orderQueue', {
     redis: {
         port: REDIS_PORT,
         host: REDIS_URI,
-        password: REDIS_PASSWORD
+        password: REDIS_PASSWORD,
     }
 })
 
