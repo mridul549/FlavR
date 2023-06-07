0666f751289491c5380f0ce24e9754a887b19d5a
Mridul Verma <94969636+mridul549@users.noreply.github.com>
2023-06-07 20:43:06 +0530

    Update timeout

--- queue/index.js
+++ queue/index.js
@@ -7,8 +7,7 @@ const client = redis.createClient({
     socket: {
         host: 'redis-10818.c273.us-east-1-2.ec2.cloud.redislabs.com',
         port: 10818
-    },
-    connect_timeout: 30000
+    }
 });
 
 client.connect();
