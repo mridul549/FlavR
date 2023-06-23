dd5e225e87b3b201e6c8eb8bafdbf1e85ef42fc0
Mridul Verma <94969636+mridul549@users.noreply.github.com>
2023-06-23 14:15:11 +0530

    Add firebase config

---
+++ config/firebase.js
@@ -0,0 +1,32 @@
+const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
+const { getFirestore, Timestamp, FieldValue, Filter } = require('firebase-admin/firestore');
+
+const serviceAccount = require('/Users/starship/Desktop/Developer stuff/Projects/FlavR/flavr-c527b-851cb0a844fd.json');
+
+initializeApp({
+    credential: cert(serviceAccount)
+});
+
+const db = getFirestore();
+
+module.exports = db
+
+
+
+
+
+// const firebase = require('firebase')
+
+// const firebaseConfig = {
+//     apiKey: process.env.FB_API_KEY,
+//     authDomain: process.env.FB_AUTH_DOMAIN,
+//     projectId: process.env.FB_PROJECT_ID,
+//     storageBucket: process.env.FB_STORAGE_BUCKET,
+//     messagingSenderId: process.env.FB_MESSAGING_SENDER_ID,
+//     appId: process.env.FB_APP_ID,
+//     measurementId: process.env.FB_MEASUREMENT_ID
+// };
+
+// const db = firebase.initializeApp(firebaseConfig)
+
+// module.exports = db
