5c1008e2da382b2b42c47341ba9ca84758460a3d
Mridul Verma <94969636+mridul549@users.noreply.github.com>
2023-06-27 16:36:10 +0530

    Add get all icons

--- routes/categoryIcon.js
+++ routes/categoryIcon.js
@@ -3,5 +3,6 @@ const router = express.Router();
 const categoryIconController = require('../controllers/categoryIconController')
 
 router.post('/addIcon', categoryIconController.addIcon)
+router.get('/getAllIcons', categoryIconController.getAllIcons)
 
 module.exports = router;
