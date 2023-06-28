0366c83bb632997c93042b7ebbf027e1d6558f1d
Mridul Verma <94969636+mridul549@users.noreply.github.com>
2023-06-28 12:55:06 +0530

    Initial category handler setup

---
+++ models/category.js
@@ -0,0 +1,30 @@
+const mongoose = require('mongoose');
+
+const categorySchema = mongoose.Schema({
+    _id: mongoose.Schema.Types.ObjectId,
+    name: {
+        type: String,
+        required: true
+    },
+    icon: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'CategoryIcon'
+    },
+    products: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            required: true,
+            ref: 'Product'
+        }
+    ],
+    outlet: {
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'Outlet'
+    }
+}, {
+    timestamps: true
+})
+
+module.exports = mongoose.model('Category', categorySchema);
--- models/categoryicon.js
+++ models/categoryicon.js
@@ -11,7 +11,7 @@ const categoryIconSchema = mongoose.Schema({
             type: String,
             required: true
         }
-    }
+    },
 }, {
     timestamps: true
 })
