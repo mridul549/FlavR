be5a690b3adbdf5d07ef01d78d2ca3f28ff925df
Mridul Verma <94969636+mridul549@users.noreply.github.com>
2024-01-23 13:05:28 +0530

    Test changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -121,7 +121,7 @@ module.exports.login = (req,res) => {
             })
         }
         const authMethod = user[0].authMethod
-        if(authMethod=="google"){
+        if(authMethod==="google"){
             return res.status(409).json({
                 message: "Password is not set for this account. Login using some other method."
             })
