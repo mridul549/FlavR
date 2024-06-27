66696ad07a0b34a2360e500e8981a26e99a7a90f
Sanyam Ratreja <sanyamratreja18@gmail.com>
2024-06-27 22:37:08 +0530

    bug fix

--- middlewares/check-auth.js
+++ middlewares/check-auth.js
@@ -2,9 +2,11 @@ const jwt = require('jsonwebtoken');
 
 module.exports = (req,res,next) => {
     try {
-        const token = req.cookies.token;
+        let token = req.cookies.token;
         if(req.headers.authorization !== undefined) {
+            console.log("check",req.headers.authorization);
             token = req.headers.authorization.split(" ")[1]
+            console.log(token)
         }
         const decoded = jwt.verify(token, process.env.TOKEN_SECRET);
         
